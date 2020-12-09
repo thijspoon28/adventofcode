fec1f6f0e784b65d232b44555aeb5e4f74f7b78c
thijspoon28 <71005565+thijspoon28@users.noreply.github.com>
2020-12-09 11:26:11 +0100

    Add files via upload

--- adventofcode day 6.1/adventofcode day 6.1/adventofcode_day_6.1.py
+++ adventofcode day 6.1/adventofcode day 6.1/adventofcode_day_6.1.py
@@ -4,12 +4,30 @@ input_file = open('input.txt', "r")  # opens the file in read mode
 
 def input_lines():
     line = True
-
-   
+    string = ''
+    list = []
+    sortlist = []
+    count = 0
     while line:
         line = input_file.readline()
         if line != "\n":
-            print(line)
+            line = line.rstrip()
+            string = line + string
+            print(string)
+        else:
+            for i in string:
+                list.append(i)
+                list.sort()
+                string = ''
+            for x in list:
+                if x != double:
+                    count += 1
+                double = x
+            print(count)
+            print(list)
+            list = []
+        double = ''
+            
 
         
 
